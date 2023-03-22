3d62f5c3e0d289da72485805e5cff7a7ce827ce3
kangbing <69076498+zhuoyue2@users.noreply.github.com>
2023-03-22 14:28:49 +0800

    181.超过经理收入的员工

---
+++ Top101-200/mysql_181.sql
@@ -0,0 +1,24 @@
+-- 超过对应经理收入的员工
+-- 表：Employee 
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- Employee 表:
+-- +----+-------+--------+-----------+
+-- | id | name  | salary | managerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | Null      |
+-- | 4  | Max   | 90000  | Null      |
+-- +----+-------+--------+-----------+
+-- Id是该表的主键。
+-- 该表的每一行都表示雇员的ID、姓名、工资和经理的ID。
+-- 编写一个SQL查询来查找收入比经理高的员工。
+
+select a.name Employee from Employee a,Employee b where (a.managerId = b.id and a.salary > b.salary)
