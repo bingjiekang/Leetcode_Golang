b5e930bce992712593d9278ed40bf6030e071006
kangbing <69076498+zhuoyue2@users.noreply.github.com>
2023-03-22 14:34:42 +0800

    183.从不订购的客户

---
+++ Top101-200/mysql_183.sql
@@ -0,0 +1,19 @@
+-- 从不订购的客户
+-- 某网站包含两个表，Customers 表和 Orders 表。编写一个 SQL 查询，找出所有从不订购任何东西的客户。
+-- Customers 表：
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders 表：
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+select Name Customers from Customers where Customers.Id not in (select CustomerId from orders)
