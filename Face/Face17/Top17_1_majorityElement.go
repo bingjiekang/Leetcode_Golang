abeebafff390b870e5e8f34ec5a3c893140aeef6
kangbing <69076498+bingjiekang@users.noreply.github.com>
2023-08-02 22:27:10 +0800

    face17.1主要元素
    通过投票算法求出最有可能的数，然后再次验证即可

---
+++ Face/Face17/Top17_1_majorityElement.go
@@ -0,0 +1,64 @@
+package main
+
+// 数组中占比超过一半的元素称之为主要元素。给你一个 整数 数组，找出其中的主要元素。若没有，返回 -1 。请设计时间复杂度为 O(N) 、空间复杂度为 O(1) 的解决方案。
+
+// 示例 1：
+
+// 输入：[1,2,5,9,5,9,5,5,5]
+// 输出：5
+
+// 示例 2：
+
+// 输入：[3,2]
+// 输出：-1
+
+func majorityElement(nums []int) int {
+	// // 定义一个map存储元素出现的次数
+	// var hash map[int]int = make(map[int]int,0)
+	// // length 存储一半的长度
+	// length := len(nums)/2
+	// // 遍历数组，记录数字出现的次数
+	// for _,v := range nums{
+	//     hash[v]++
+	// }
+	// // 遍历map，如果有大于一半的数组值则返回，否则返回-1
+	// for k,v := range hash{
+	//     if v>length{
+	//         return k
+	//     }
+	// }
+	// return -1
+
+	// 投票法
+	// 由于求出现超过一半长度的数，遍历每个数，记录这个数num和出现的次数，如果count为0，则赋值这个数为num，如果num等于这个数，则count++，不等于则cout--，由于如果存在超过一半长度的数，那么这个数一定会和其他数消完后还余1，如果为空则不存在，还有一种情况是奇数次且不存在超过数组长度一半的数，所以需要第二次遍历查看这个数是否合理
+	// num记录这个数，cout记录出现的次数，length记录数组长度的一半
+	var num, cout, length int
+	length = len(nums) / 2
+	// 遍历这个数组，出现次数为0，赋值num和对cout++,大于0则判断是否相同相同cout++，不同cout--
+	for _, v := range nums {
+		if cout == 0 {
+			num = v
+			cout++
+		} else {
+			if num == v {
+				cout++
+			} else {
+				cout--
+			}
+		}
+	}
+	// 出现次数大于0，判断这个数出现的次数是否真的超过数组的一半
+	if cout > 0 {
+		cout = 0
+		for _, v := range nums {
+			if v == num {
+				cout++
+			}
+		}
+		if cout > length {
+			return num
+		}
+	}
+	return -1
+
+}
--- try.go
+++ try.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 // "strconv"
@@ -9,36 +11,71 @@ import (
 // "math"
 
 func main() {
+	// var hash map[string]int = make(map[string]int, 0)
+	// n, m := 1, 2
+	// temp := strconv.Itoa(n) + "," + strconv.Itoa(m)
+	// fmt.Println(temp)
+	// hash[temp] = 1
+	// fmt.Println(hash)
+	// // var su []string = make([]string, 0)
+	// su := strings.Split(temp, ",")
+	// t1, _ := strconv.ParseInt(su[0], 10, 64)
+	// t2, _ := strconv.ParseInt(su[0], 10, 64)
+	// var tm [4][7]int = [4][7]int{
 
-	// n := 3
-
-	// for i := 0; i <= 4; i++ {
-	// 	switch i {
-	// 	case 3:
-	// 		fmt.Println(3)
-	// 	default:
-	// 		fmt.Println(99999)
-	// 	}
+	// 	[14, 15, 100, 9, 3],
+	// 	[32, 1, 9, 3, 5],
+	// 	[15, 29, 2, 6, 8, 7],
+	// 	[7, 10]
 
 	// }
-	// num1 := "3"
-	// s1 := []rune(num1)
-	// num2 := "5"
-	// s2 := []rune(num2)
-	// fmt.Println(s1 * s2)
 
 }
 
-func singleNumber(nums []int) int {
-	var temp map[int]int = make(map[int]int, 0)
-	for _, v := range nums {
-		temp[v]++
-		delete(temp, v)
-	}
-	for i, v := range temp {
-		if v == 1 {
-			return i
+func computeSimilarities(docs [][]int) (sult []string) {
+	// 用map型数组存储每组数组对应的组数数组
+	length := len(docs)
+	var curd map[int][]int = make(map[int][]int, 0)
+	for i, v := range docs {
+		for _, w := range v {
+			// 判断数w是否已经在map1里
+			_, ok := curd[w]
+			if !ok { // 如果不在则声明一个
+				curd[w] = make([]int, length+1, length+2)
+			}
+			// 表示这个数所在的组为1
+			curd[w][i] = 1
+			// 最后一个数表示这个数对应组里有几个数
+			curd[w][length]++
 		}
 	}
-	return 0
+	// 定义一个map2用来存储对应两组出现的次数,表示交集
+	// var hash map[string]int = make(map[string]int, 0)
+	// 定义一个二维切片存储对应的次数
+	var lt [][]int = make([][]int, length, length+1)
+	// 遍历每个map1,提取对应两组出现的组数 对其进行加1表示交集数
+	for _, v := range curd {
+		if v[length] > 1 {
+			for i := 0; i < length-1; i++ {
+				for j := i + 1; j < length; j++ {
+					lt[i] = make([]int, length, length+1)
+					if v[i] == 1 && v[j] == 1 {
+						lt[i][j]++
+					}
+				}
+			}
+		}
+	}
+	clear(curd)
+	// 遍历每个数组获取对应数组下标的长度 相加-交集即为并集,想除并转换加入即可
+	for i := 0; i < length-1; i++ {
+		for j := i + 1; j < length; j++ {
+			// 求对应并集
+			ad := len(docs[i]) + len(docs[j]) - lt[i][j]
+			ku := float64(lt[i][j])/float64(ad) + 1e-9
+			tsu := fmt.Sprintf("%d,%d: %.4f", i, j, ku)
+			sult = append(sult, tsu)
+		}
+	}
+	return
 }
