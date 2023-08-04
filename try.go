725694f237fb06e876b70fdf1e4cb2378d405cb7
kangbing <69076498+bingjiekang@users.noreply.github.com>
2023-08-04 20:10:24 +0800

    face_7__22c删除注释
    通过判断是否在注释块内来判断是否要删除它

--- try.go
+++ try.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 // "strconv"
@@ -11,71 +9,43 @@ import (
 // "math"
 
 func main() {
-	// var hash map[string]int = make(map[string]int, 0)
-	// n, m := 1, 2
-	// temp := strconv.Itoa(n) + "," + strconv.Itoa(m)
-	// fmt.Println(temp)
-	// hash[temp] = 1
-	// fmt.Println(hash)
-	// // var su []string = make([]string, 0)
-	// su := strings.Split(temp, ",")
-	// t1, _ := strconv.ParseInt(su[0], 10, 64)
-	// t2, _ := strconv.ParseInt(su[0], 10, 64)
-	// var tm [4][7]int = [4][7]int{
-
-	// 	[14, 15, 100, 9, 3],
-	// 	[32, 1, 9, 3, 5],
-	// 	[15, 29, 2, 6, 8, 7],
-	// 	[7, 10]
-
-	// }
+	st := []string{"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"}
+	fmt.Println(removeComments(st))
 
 }
 
-func computeSimilarities(docs [][]int) (sult []string) {
-	// 用map型数组存储每组数组对应的组数数组
-	length := len(docs)
-	var curd map[int][]int = make(map[int][]int, 0)
-	for i, v := range docs {
-		for _, w := range v {
-			// 判断数w是否已经在map1里
-			_, ok := curd[w]
-			if !ok { // 如果不在则声明一个
-				curd[w] = make([]int, length+1, length+2)
-			}
-			// 表示这个数所在的组为1
-			curd[w][i] = 1
-			// 最后一个数表示这个数对应组里有几个数
-			curd[w][length]++
-		}
-	}
-	// 定义一个map2用来存储对应两组出现的次数,表示交集
-	var hash map[string]int = make(map[string]int, 0)
-	// 遍历每个map1,提取对应两组出现的组数 对其进行加1表示交集数
-	for _, v := range curd {
-		if v[length] > 1 {
-			for i := 0; i < length-1; i++ {
-				for j := i + 1; j < length; j++ {
-					if v[i] == 1 && v[j] == 1 {
-						temp := strconv.Itoa(i) + "," + strconv.Itoa(j)
-						hash[temp]++
-					}
+// 两大类：在块内和不在块内，用bloak进行标记
+func removeComments(source []string) (sult []string) {
+	// 定义bloak用来记录是否在标注内
+	var bloak bool = false
+	// 当检查到// 时直接结束这个
+	temp := []byte{}
+	for _, v := range source {
+		// var temp []byte = make([]byte, 0)
+
+		length := len(v)
+		// 当检查到/* bloak进行标记，直到*/进行解除
+		for i := 0; i < length; i++ {
+			if i < length-1 && v[i] == '/' && v[i+1] == '/' && bloak == false {
+				break
+			} else if i < length-1 && v[i] == '/' && v[i+1] == '*' {
+				i++
+				bloak = true
+			} else if i < length-1 && bloak && v[i] == '*' && v[i+1] == '/' {
+				i++
+				bloak = false
+			} else {
+				if !bloak {
+					temp = append(temp, v[i])
 				}
 			}
 		}
-	}
-	// 遍历每个map2获取对应数组的长度 相加-交集即为并集,想除并转换加入即可
-	for k, v := range hash {
-		var tsu string
-		su := strings.Split(k, ",")
-		t1, _ := strconv.ParseInt(su[0], 10, 64)
-		t2, _ := strconv.ParseInt(su[1], 10, 64)
-		ad := len(docs[t1]) + len(docs[t2]) - v
-		ku := float64(v)/float64(ad) + 1e-9
-		if ku > 0 {
-			tsu = k + ": " + fmt.Sprintf("%.4f", ku)
+		ltmp := string(temp)
+		if ltmp != "" && !bloak {
+			sult = append(sult, ltmp)
+			temp = []byte{}
 		}
-		sult = append(sult, tsu)
+
 	}
 	return
 }
